f6bafe098fb33e8a25460c33b8551ea23725cd91
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-02-29 19:40:13 +0600

    Renamed mongoose db

--- api/config.ts
+++ api/config.ts
@@ -7,7 +7,7 @@ const config = {
     rootPath,
     publicPath: path.join(rootPath, 'public'),
     mongoose: {
-        db: 'mongodb://localhost/music',
+        db: 'mongodb://localhost/forum',
     }
 };
 
